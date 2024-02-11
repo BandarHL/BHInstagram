05e8d9cd92e3e50f8987e5917cd762813df2bef0
N0xv <87769466+nv0xv@users.noreply.github.com>
2024-02-11 19:49:07 +0400

    v0.0.2

--- SettingsViewController.m
+++ SettingsViewController.m
@@ -35,7 +35,7 @@
 }
 - (PSSpecifier *)newSwitchCellWithTitle:(NSString *)titleText detailTitle:(NSString *)detailText key:(NSString *)keyText defaultValue:(BOOL)defValue changeAction:(SEL)changeAction {
     PSSpecifier *switchCell = [PSSpecifier preferenceSpecifierNamed:titleText target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSSwitchCell edit:nil];
-    
+
     [switchCell setProperty:keyText forKey:@"key"];
     [switchCell setProperty:keyText forKey:@"id"];
     [switchCell setProperty:@YES forKey:@"big"];
@@ -50,7 +50,7 @@
 }
 - (PSSpecifier *)newButtonCellWithTitle:(NSString *)titleText detailTitle:(NSString *)detailText dynamicRule:(NSString *)rule action:(SEL)action {
     PSSpecifier *buttonCell = [PSSpecifier preferenceSpecifierNamed:titleText target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSButtonCell edit:nil];
-    
+
     [buttonCell setButtonAction:action];
     [buttonCell setProperty:@YES forKey:@"big"];
     [buttonCell setProperty:BHButtonTableViewCell.class forKey:@"cellClass"];
@@ -65,7 +65,7 @@
 }
 - (PSSpecifier *)newHBLinkCellWithTitle:(NSString *)titleText detailTitle:(NSString *)detailText url:(NSString *)url {
     PSSpecifier *HBLinkCell = [PSSpecifier preferenceSpecifierNamed:titleText target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:PSButtonCell edit:nil];
-    
+
     [HBLinkCell setButtonAction:@selector(hb_openURL:)];
     [HBLinkCell setProperty:HBLinkTableCell.class forKey:@"cellClass"];
     [HBLinkCell setProperty:url forKey:@"url"];
@@ -76,7 +76,7 @@
 }
 - (PSSpecifier *)newHBTwitterCellWithTitle:(NSString *)titleText twitterUsername:(NSString *)user customAvatarURL:(NSString *)avatarURL {
     PSSpecifier *TwitterCell = [PSSpecifier preferenceSpecifierNamed:titleText target:self set:@selector(setPreferenceValue:specifier:) get:@selector(readPreferenceValue:) detail:nil cell:1 edit:nil];
-    
+
     [TwitterCell setButtonAction:@selector(hb_openURL:)];
     [TwitterCell setProperty:HBTwitterCell.class forKey:@"cellClass"];
     [TwitterCell setProperty:user forKey:@"user"];
@@ -87,13 +87,13 @@
 }
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        
+
         PSSpecifier *feedSection = [self newSectionWithTitle:@"Feed" footer:nil];
         PSSpecifier *mediaSection = [self newSectionWithTitle:@"Save media" footer:nil];
         PSSpecifier *storySection = [self newSectionWithTitle:@"Story and direct" footer:nil];
         PSSpecifier *securitySection = [self newSectionWithTitle:@"Security" footer:nil];
         PSSpecifier *developer = [self newSectionWithTitle:@"Developer" footer:nil];
-        
+
         PSSpecifier *hideAds = [self newSwitchCellWithTitle:@"Hide Ads" detailTitle:@"Remove all Ads from Instagram app" key:@"hide_ads" defaultValue:true changeAction:nil];
         PSSpecifier *noSuggestedPost = [self newSwitchCellWithTitle:@"No suggested post" detailTitle:@"Remove suggested posts from the feed" key:@"no_suggested_post" defaultValue:false changeAction:nil];
         PSSpecifier *showLikeCount = [self newSwitchCellWithTitle:@"Show Like count" detailTitle:@"Show like count in the post" key:@"show_like_count" defaultValue:true changeAction:nil];
@@ -115,9 +115,9 @@
         // dvelopers section
         PSSpecifier *bandarHL = [self newHBTwitterCellWithTitle:@"BandarHelal" twitterUsername:@"BandarHL" customAvatarURL:@"https://unavatar.io/twitter/BandarHL"];
         PSSpecifier *tipJar = [self newHBLinkCellWithTitle:@"Tip Jar" detailTitle:@"Donate Via Paypal" url:@"https://www.paypal.me/BandarHL"];
-        
+        PSSpecifier *sourcecode = [self newHBLinkCellWithTitle:@"Source Code" detailTitle:@"BHInsta Source Code on Github" url:@"https://github.com/BandarHL/BHInstagram"];
         _specifiers = [NSMutableArray arrayWithArray:@[
-            
+
             feedSection, // 1
             hideAds,
             noSuggestedPost,
@@ -140,32 +140,34 @@
 
             securitySection, // 4
             appLock,
-            
+
             developer, // 5
             bandarHL,
             tipJar,
+            sourcecode,
+            
         ]];
-        
+
         [self collectDynamicSpecifiersFromArray:_specifiers];
     }
-    
+
     return _specifiers;
 }
 - (void)reloadSpecifiers {
     [super reloadSpecifiers];
-    
+
     [self collectDynamicSpecifiersFromArray:self.specifiers];
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (self.hasDynamicSpecifiers) {
         PSSpecifier *dynamicSpecifier = [self specifierAtIndexPath:indexPath];
         BOOL __block shouldHide = false;
-        
+
         [self.dynamicSpecifiers enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
             NSMutableArray *specifiers = obj;
             if ([specifiers containsObject:dynamicSpecifier]) {
                 shouldHide = [self shouldHideSpecifier:dynamicSpecifier];
-                
+
                 UITableViewCell *specifierCell = [dynamicSpecifier propertyForKey:PSTableCellKey];
                 specifierCell.clipsToBounds = shouldHide;
             }
@@ -174,43 +176,43 @@
             return 0;
         }
     }
-    
+
     return UITableViewAutomaticDimension;
 }
 
 - (void)collectDynamicSpecifiersFromArray:(NSArray *)array {
     if (!self.dynamicSpecifiers) {
         self.dynamicSpecifiers = [NSMutableDictionary new];
-        
+
     } else {
         [self.dynamicSpecifiers removeAllObjects];
     }
-    
+
     for (PSSpecifier *specifier in array) {
         NSString *dynamicSpecifierRule = [specifier propertyForKey:@"dynamicRule"];
-        
+
         if (dynamicSpecifierRule.length > 0) {
             NSArray *ruleComponents = [dynamicSpecifierRule componentsSeparatedByString:@", "];
-            
+
             if (ruleComponents.count == 3) {
                 NSString *opposingSpecifierID = [ruleComponents objectAtIndex:0];
                 if ([self.dynamicSpecifiers objectForKey:opposingSpecifierID]) {
                     NSMutableArray *specifiers = [[self.dynamicSpecifiers objectForKey:opposingSpecifierID] mutableCopy];
                     [specifiers addObject:specifier];
-                    
-                    
+
+
                     [self.dynamicSpecifiers removeObjectForKey:opposingSpecifierID];
                     [self.dynamicSpecifiers setObject:specifiers forKey:opposingSpecifierID];
                 } else {
                     [self.dynamicSpecifiers setObject:[NSMutableArray arrayWithArray:@[specifier]] forKey:opposingSpecifierID];
                 }
-                
+
             } else {
                 [NSException raise:NSInternalInconsistencyException format:@"dynamicRule key requires three components (Specifier ID, Comparator, Value To Compare To). You have %ld of 3 (%@) for specifier '%@'.", ruleComponents.count, dynamicSpecifierRule, [specifier propertyForKey:PSTitleKey]];
             }
         }
     }
-    
+
     self.hasDynamicSpecifiers = (self.dynamicSpecifiers.count > 0);
 }
 - (DynamicSpecifierOperatorType)operatorTypeForString:(NSString *)string {
@@ -221,53 +223,53 @@
     if (specifier) {
         NSString *dynamicSpecifierRule = [specifier propertyForKey:@"dynamicRule"];
         NSArray *ruleComponents = [dynamicSpecifierRule componentsSeparatedByString:@", "];
-        
+
         PSSpecifier *opposingSpecifier = [self specifierForID:[ruleComponents objectAtIndex:0]];
         id opposingValue = [self readPreferenceValue:opposingSpecifier];
         id requiredValue = [ruleComponents objectAtIndex:2];
-        
+
         if ([opposingValue isKindOfClass:NSNumber.class]) {
             DynamicSpecifierOperatorType operatorType = [self operatorTypeForString:[ruleComponents objectAtIndex:1]];
-            
+
             switch (operatorType) {
                 case EqualToOperatorType:
                     return ([opposingValue intValue] == [requiredValue intValue]);
                     break;
-                    
+
                 case NotEqualToOperatorType:
                     return ([opposingValue intValue] != [requiredValue intValue]);
                     break;
-                    
+
                 case GreaterThanOperatorType:
                     return ([opposingValue intValue] > [requiredValue intValue]);
                     break;
-                    
+
                 case LessThanOperatorType:
                     return ([opposingValue intValue] < [requiredValue intValue]);
                     break;
             }
         }
-        
+
         if ([opposingValue isKindOfClass:NSString.class]) {
             return [opposingValue isEqualToString:requiredValue];
         }
-        
+
         if ([opposingValue isKindOfClass:NSArray.class]) {
             return [opposingValue containsObject:requiredValue];
         }
     }
-    
+
     return NO;
 }
 
 - (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
     NSUserDefaults *Prefs = [NSUserDefaults standardUserDefaults];
     [Prefs setValue:value forKey:[specifier identifier]];
-    
+
     if (self.hasDynamicSpecifiers) {
         NSString *specifierID = [specifier propertyForKey:PSIDKey];
         PSSpecifier *dynamicSpecifier = [self.dynamicSpecifiers objectForKey:specifierID];
-        
+
         if (dynamicSpecifier) {
             [self.table beginUpdates];
             [self.table endUpdates];
@@ -311,7 +313,7 @@
         self.detailTextLabel.text = subTitle;
         self.detailTextLabel.numberOfLines = isBig ? 0 : 1;
         self.detailTextLabel.textColor = [UIColor secondaryLabelColor];
-        
+
         if (specifier.properties[@"switchAction"]) {
             UISwitch *targetSwitch = ((UISwitch *)[self control]);
             NSString *strAction = [specifier.properties[@"switchAction"] copy];
