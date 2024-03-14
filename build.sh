eeb3f6816e9520119a406e0f7014ec0fdd3ce568
SoCuul <63339559+SoCuul@users.noreply.github.com>
2024-03-13 23:50:09 -0700

    Refactor: Build script now looks for any instagram-ish ipa in packages

--- build.sh
+++ build.sh
@@ -9,12 +9,15 @@ make clean
 rm -rf .theos
 make
 
-if [ -e ./packages/com.burbn.instagram.ipa ]; then
+# IPA File
+ipaFile="$(find ./packages/*com.burbn.instagram*.ipa -type f -exec basename {} \;)"
+
+if [ -n "${ipaFile}" ]; then
 
   echo -e '\033[1m\033[32mBuilding the IPA.\033[0m'
-  azule -i "$PROJECT_PATH/packages/com.burbn.instagram.ipa" -o "$PROJECT_PATH/packages" -n BHInsta-sideloaded -r -f "$PROJECT_PATH/.theos/obj/debug/BHInsta.dylib" "$PROJECT_PATH/packages/Cephei.framework" "$PROJECT_PATH/packages/CepheiUI.framework" "$PROJECT_PATH/packages/CepheiPrefs.framework" "$PROJECT_PATH/modules/libflex/.theos/obj/debug/libbhFLEX.dylib"
+  azule -i "$PROJECT_PATH/packages/${ipaFile}" -o "$PROJECT_PATH/packages" -n BHInsta-sideloaded -r -f "$PROJECT_PATH/.theos/obj/debug/BHInsta.dylib" "$PROJECT_PATH/packages/Cephei.framework" "$PROJECT_PATH/packages/CepheiUI.framework" "$PROJECT_PATH/packages/CepheiPrefs.framework" "$PROJECT_PATH/modules/libflex/.theos/obj/debug/libbhFLEX.dylib"
 
   echo -e "\033[1m\033[32mDone, we hope you enjoy BHInsta!\033[0m\n\nYou can find the ipa file at: $PROJECT_PATH/packages"
 else
-  echo -e '\033[1m\033[0;31mpackages/com.burbn.instagram.ipa not found.\nPlease put a decrypted Instagram IPA in its path.\033[0m'
+  echo -e '\033[1m\033[0;31m./packages/com.burbn.instagram.ipa not found.\nPlease put a decrypted Instagram IPA in its path.\033[0m'
 fi
